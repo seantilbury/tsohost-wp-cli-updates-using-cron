e35d70ef93b70269f5d726d1ec300e89c3d7e3cc
Sean Tilbury <sean@tilbury.org>
2017-10-14 22:03:32 +0100

    Update wpcli.sh
    add update wp-cli version

--- wpcli.sh
+++ wpcli.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 wp="/usr/bin/php-5.6-cli /var/sites/y/yourdomain.co.uk/wp-cli.phar"
 cd /var/sites/y/yourdomain.co.uk/public_html/
+${wp} cli update
 ${wp} plugin update --all
 ${wp} theme update --all
 ${wp} core update
