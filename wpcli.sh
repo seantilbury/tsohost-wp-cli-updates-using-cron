fd5ee543555286c975e16bde10c7b855911e1d83
Sean Tilbury <sean@tilbury.org>
2017-10-19 14:40:18 +0100

    Update wpcli.sh

--- wpcli.sh
+++ wpcli.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 wp="/usr/bin/php-5.6-cli /var/sites/y/yourdomain.co.uk/wp-cli.phar"
 cd /var/sites/y/yourdomain.co.uk/public_html/
-${wp} cli update
 ${wp} plugin update --all
 ${wp} theme update --all
 ${wp} core update
