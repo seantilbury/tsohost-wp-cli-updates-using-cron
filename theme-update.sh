1cce9c463d7669286ad8b9852290a888be20de47
Sean Tilbury <sean@tilbury.org>
2017-12-07 22:06:49 +0000

    Update theme-update.sh
    add php.ini

--- theme-update.sh
+++ theme-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set path to wpcli #
-wp="/usr/bin/php-5.6-cli ./wp-cli.phar"
+wp="/usr/bin/php-5.6-cli -c ./php.ini ./wp-cli.phar"
 
 # update all themes #
 ${wp} theme update --all
